7b5ac1c80506020dff77bef1df9b5136dc59f0ad
Ethan Payne <ethanp@internode.on.net>
2022-05-11 11:31:07 +1000

    Added basic command line game for deathrolling between two players

---
+++ deathroll_game.py
@@ -0,0 +1,37 @@
+"""
+A simple console-based script where two players can "Death Roll" with a custom initial upper bound. Player 1 goes first.
+Last Modified: 11/05/2022
+By SlyZorua
+"""
+
+# Imports
+from random import randint
+
+# Set current roll, and player 1 goes first
+current_roll = int(input("Select initial roll maximum: "))
+current_player = 1
+
+while True:
+    # Roll 1-current_roll
+    current_roll = randint(1,current_roll)
+    print("Player " + str(current_player) + " rolled a: " + str(current_roll))
+    
+    # Break if player rolls a 1
+    if current_roll == 1:
+        break
+    
+    # Next players turn
+    if current_player == 1:
+        current_player = 2
+    elif current_player == 2:
+        current_player = 1
+
+# Declare winner as the opposite of the player that rolled 1
+if current_player == 1:
+    winner = 2
+elif current_player == 2:
+    winner = 1
+
+print("Player " + str(current_player) + " rolled a 1! Player " + str(winner) + " wins!")
+
+i = input("Press any key to exit")
